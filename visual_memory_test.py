f81b684e301c80fae528b41fd773c3871f5f7033
Josh H <josh_holtz@starkey.com>
2022-08-18 22:20:21 -0500

    Started verbal memory file

--- visual_memory_test.py
+++ visual_memory_test.py
@@ -17,5 +17,7 @@ def run(driver, stop_score):
         EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div[4]/div[1]/div/div/div/div[2]/div"))
     )
     
+    active = []
     for row in grid.find_elements(by="tag name", value="div"):
         for cell in row.find_elements(by="tag name", value="div"):
+            
